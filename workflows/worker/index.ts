00011cfbc6dd3757c2e9ae985543ef25ff66fd3f
pjh5474 <warwarsn@gmail.com>
2026-09-13 17:40:48 +0900

    workflow waitForEvent, sendEvent

--- workflows/worker/index.ts
+++ workflows/worker/index.ts
@@ -11,6 +11,12 @@ type Params = {
 	agentName: string;
 };
 
+type EventPayload = {
+	note: string;
+	eta: number;
+	approved: boolean;
+};
+
 export class PizzaWorkflow extends WorkflowEntrypoint<Env, Params> {
 	async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
 		const reportProgress = async (patch: Partial<Order>) => {
@@ -23,6 +29,86 @@ export class PizzaWorkflow extends WorkflowEntrypoint<Env, Params> {
 
 		await reportProgress({
 			stage: "awaiting-approval",
+		});
+
+		let decision;
+		try {
+			decision = await step.waitForEvent<EventPayload>("decide-approval", {
+				timeout: "30 seconds",
+				type: "potato-approval",
+			});
+		} catch (e) {
+			console.error(e);
+			await reportProgress({
+				stage: "rejected",
+			});
+			return;
+		}
+
+		if (!decision?.payload.approved) {
+			await reportProgress({
+				stage: "rejected",
+			});
+			return;
+		}
+
+		await reportProgress({
+			etaMinutes: decision?.payload.eta,
+			note: decision?.payload.note,
+			stage: "paying",
+		});
+
+		let attempt = 0;
+		try {
+			await step.do(
+				"charge",
+				{
+					retries: {
+						limit: 10,
+						delay: "5 seconds",
+						backoff: "constant",
+					},
+				},
+				async () => {
+					await reportProgress({
+						chargeAttempts: attempt,
+					});
+					attempt++;
+					if (Math.random() < 0.8) throw new Error("Card declined");
+
+					return {
+						chargedAt: Date.now(),
+					};
+				},
+			);
+		} catch (e) {
+			console.log(e);
+			await reportProgress({
+				stage: "rejected",
+			});
+			return;
+		}
+
+		await reportProgress({
+			stage: "preparing",
+		});
+
+		await step.sleep("preparing sleep", "10 seconds");
+
+		await reportProgress({
+			stage: "baking",
+		});
+
+		await step.sleep("baking sleep", "10 seconds");
+
+		await reportProgress({
+			stage: "delivering",
+		});
+
+		await step.sleep("delivering sleep", "10 seconds");
+
+		await reportProgress({
+			stage: "delivered",
 		});
 	}
 }
@@ -57,6 +143,30 @@ export class RestaurantAgent extends Agent<Env, State> {
 					...this.state.orders[orderId],
 					...patch,
 				},
+			},
+		});
+	}
+
+	@callable()
+	async approveOrder(orderId: string, eta: number, note: string) {
+		const instance = await this.env.PIZZA_WORKFLOW.get(orderId);
+		await instance.sendEvent({
+			type: "potato-approval",
+			payload: {
+				note,
+				eta,
+				approved: true,
+			},
+		});
+	}
+
+	@callable()
+	async rejectOrder(orderId: string) {
+		const instance = await this.env.PIZZA_WORKFLOW.get(orderId);
+		await instance.sendEvent({
+			type: "potato-approval",
+			payload: {
+				approved: false,
 			},
 		});
 	}
